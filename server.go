ffff848f95b04162da932fd3623e589c4f5303d1
aiirononeko <aiirononeko@gmail.com>
2022-11-23 15:25:33 +0900

    create db connection

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"mameuri-graphql-server/graph"
 	"mameuri-graphql-server/graph/generated"
@@ -9,15 +11,40 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/lib/pq"
 )
 
-const defaultPort = "8080"
+const (
+	HOST         = "127.0.0.1"
+	DATABASE     = "mameuri_dev"
+	USER         = "postgres"
+	PASSWORD     = "postgres"
+	DEFAULT_PORT = "8080"
+)
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
+
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = defaultPort
+		port = DEFAULT_PORT
 	}
+
+	// Initialize connection string.
+	var connectionString string = fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", HOST, USER, PASSWORD, DATABASE)
+
+	// Initialize connection object.
+	db, err := sql.Open("postgres", connectionString)
+	checkError(err)
+
+	err = db.Ping()
+	checkError(err)
+	fmt.Println("Successfully created connection to database")
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
